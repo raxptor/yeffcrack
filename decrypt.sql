5f089c6553fa3599ed213b4072d12fa4cc4d2a44
Dan Hemma <dan@konunger.com>
2023-01-24 10:22:57 +0100

    - updated sql

--- decrypt.sql
+++ decrypt.sql
@@ -1,14 +1,14 @@
 BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS "decrypt" (
 	"uncracked"	TEXT NOT NULL,
-	"eval"	NUMERIC NOT NULL,
 	"length"	INTEGER NOT NULL,
+	"eval"	NUMERIC NOT NULL,
+	"penalty"	INTEGER NOT NULL DEFAULT 0,
 	"quadgram_rating"	NUMERIC,
 	"cracked"	TEXT,
 	"alphabet"	TEXT,
 	"steps"	TEXT,
 	"freq_rating"	INTEGER,
-	"is_test_data"	INTEGER DEFAULT 0,
 	"touched"	INTEGER NOT NULL DEFAULT 0,
 	PRIMARY KEY("uncracked")
 );
